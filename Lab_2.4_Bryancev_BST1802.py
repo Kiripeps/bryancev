aa970bfb7c281b4af3d12abd8946b3ab74f85c4c
Kiripeps <47979082+Kiripeps@users.noreply.github.com>
2020-05-13 10:28:17 +0300

    Delete Lab_2.4_Bryancev_BST1802.py

--- Lab_2.4_Bryancev_BST1802.py
+++
@@ -1,9 +0,0 @@
-#Задание 4
-userList = list(input("Введите элементы списка через пробел \n").split())
-print(len(userList))
-del userList[0:2]
-
-newList = list(input("Введите новые элементы через пробел \n").split())
-userList = userList + newList
-print (userList)
-
